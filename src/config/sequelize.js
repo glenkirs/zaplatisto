5c3ffe66858a7a31db3445c762c2e378004a7459
k.zhoglenko <k.zhoglenko@2-berega.ru>
2022-10-09 17:29:48 +0500

    Добавили определение наличия пользователя

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -4,10 +4,10 @@ const logger = require('../helpers/logger').getLogger();
 const { _ } = require('lodash');
 
 module.exports = {
-  username: 'zaplatista_dev',
-  password: 'HiNvkvz6GryFXM6rtHnN',
-  database: 'zaplatisto',
-  host: 'database',
+  username: env.SEQUELIZE_USER || 'zaplatisto',
+  password: env.SEQUELIZE_PASS || 'zaplatisto',
+  database: env.SEQUELIZE_DB || 'zaplatisto',
+  host: env.SEQUELIZE_HOST || 'database',
   port: env.SEQUELIZE_PORT || '3306',
   dialect: env.SEQUELIZE_DIALECT || 'mysql',
   log: env.SEQUELIZE_LOG || 'enabled',
