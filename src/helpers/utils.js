b0052a8d8093fee4c05e1cde10e9835ca81ae99b
k.zhoglenko <k.zhoglenko@2-berega.ru>
2022-11-23 11:51:06 +0500

    Create mailserver networks

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -205,14 +205,16 @@ const createEmailAccount = async (account, user) => {
                 pass: config.email.password
             },
             form: {
-                name: user.name || '',
-                user: account.login.replace(/@[A-Z0-9.-]+\.[A-Z]{2,4}/i, ''),
-                domain: config.email.domain,
-                passwordPlaintext: pass,
-                superAdmin: 0,
-                discard: 0,
-                strictFromDisabled: 0,
-                domainAdmin: 0
+                children: {
+                    name: user.name || '',
+                    user: account.login.replace(/@[A-Z0-9.-]+\.[A-Z]{2,4}/i, ''),
+                    domain: config.email.domain,
+                    passwordPlaintext: pass,
+                    superAdmin: 0,
+                    discard: 0,
+                    strictFromDisabled: 0,
+                    domainAdmin: 0
+                }
             },
             uri: `${config.services.mail}/admin/api/v1/boxes`,
             method: 'POST',
